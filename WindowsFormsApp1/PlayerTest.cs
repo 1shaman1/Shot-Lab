0a2bdbcd09bcb8a4e444c108460d92ba482599f2
1shaman1 <79924068+1shaman1@users.noreply.github.com>
2021-05-19 20:29:37 +0500

    Add files via upload

---
+++ WindowsFormsApp1/PlayerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+namespace ShotLab
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void WeaponChange()
+        {
+            var expectedWeapon = new Weapon(500, 30, WeaponType.Knife);
+            var player = new Player(100, new Point(0, 0));
+            player.ChangeWeapon();
+            Assert.AreEqual(expectedWeapon, player.CurrentWeapon);
+        }
+    }
+}
---
+++ WindowsFormsApp1/WeaponTests.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Drawing;
+//using NUnit.Framework;
+
+//namespace ShotLab
+//{
+//    [TestFixture]
+//    public class WeaponTests
+//    {
+//        [Test]
+//        public void WeaponChange()
+//        {
+//            var expectedWeapon = new Weapon(500, 30, WeaponType.Knife);
+//            var weapon = new Weapon();
+//            weapon.ChangeWeapon();
+//            Assert.AreEqual(expectedWeapon, weapon);
+//        }
+//    }
+//}
